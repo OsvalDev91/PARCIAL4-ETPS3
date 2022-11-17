2eef92cb31c0a38b844c3ce1a00bc44a42c4144d
Osvaldo Guzman <2505202015@mail.utec.edu.sv>
2022-11-17 15:30:58 -0600

    tercer commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:parcial4etps/principal.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(PrimeraPantalla());
+  runApp(FirebaseAPP());
 }
 
 class FirebaseAPP extends StatelessWidget {
@@ -16,13 +16,13 @@ class FirebaseAPP extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Firebase Firestore',
-      //home: Principal(),
+      title: 'PARCIAL4 ETPS3',
+      home: Principal(),
     );
   }
 }
 
-class PrimeraPantalla extends StatelessWidget {
+/*class PrimeraPantalla extends StatelessWidget {
   const PrimeraPantalla({Key? key}) : super(key: key);
 
   @override
@@ -36,4 +36,4 @@ class PrimeraPantalla extends StatelessWidget {
       ),
     );
   }
-}
+}*/
